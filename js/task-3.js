860b95c2accedfaf51ca663b476cd3eaa21f99ee
Serhii Hudzenko <gudzsvmail@gmail.com>
2023-12-30 20:01:25 +0200

    goit-js-hw-03 complete to check

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function filterArray(numbers, value) {
+  const arr = [];
+  for (const i of numbers) {
+    if (i > value) arr.push(i);
+  }
+  return arr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
